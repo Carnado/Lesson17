2a5ae1bd82507986a96d593b82a4c93732efce1a
Ibragimov <sherzodibragimov1998@gmail.com>
2023-04-27 14:42:36 +0500

    Bu hali tugalannmadi!!!!

--- app/src/main/java/uz/fozilbek/lesson17/ui/EditActivity.kt
+++ app/src/main/java/uz/fozilbek/lesson17/ui/EditActivity.kt
@@ -1,31 +1,42 @@
 package uz.fozilbek.lesson17.ui
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
-import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.forEachIndexed
 import com.google.android.material.imageview.ShapeableImageView
+import uz.fozilbek.lesson17.R
 import uz.fozilbek.lesson17.core.cache.UserCache
 import uz.fozilbek.lesson17.core.data.UserModel
-import uz.fozilbek.lesson17.databinding.ActivityUserAddBinding
-import java.io.ByteArrayOutputStream
+import uz.fozilbek.lesson17.databinding.ActivityEditBinding
 
-
-class UserAddActivity : AppCompatActivity() {
-    lateinit var binding: ActivityUserAddBinding
-    var choseImage = 1;
-    var choseColor = "#1dd1a1"
+class EditActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditBinding
+    var choseColor=""
+    var name=""
+    var followers=0
+    var choseImage=1
+    var nameUser="No Name"
+    var followerUser=0
+    var imageUser=1;
+    var backgroundUser="#48dbfb"
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityUserAddBinding.inflate(layoutInflater)
+        binding= ActivityEditBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         loadActions()
+        loadData()
+    }
+
+    private fun loadData() {
+        name=intent.getStringExtra("username")!!
+        followers=intent.getIntExtra("user_followers",0)!!
+        binding.userName.setText(name)
+        binding.userFollowers.setText(followers.toString())
     }
 
     private fun loadActions() {
+
         binding.imageGroup.forEachIndexed { index, view ->
             view.setOnClickListener {
                 binding.userIcon.setImageDrawable((view as ShapeableImageView).drawable)
@@ -58,18 +69,23 @@ class UserAddActivity : AppCompatActivity() {
                     binding.color1 -> {
                         choseColor = "#48dbfb"
                     }
+
                     binding.color2 -> {
                         choseColor = "#10ac84"
                     }
+
                     binding.color3 -> {
                         choseColor = "#ff9f43"
                     }
+
                     binding.color4 -> {
                         choseColor = "#5f27cd"
                     }
+
                     binding.color5 -> {
                         choseColor = "#576574"
                     }
+
                     binding.color6 -> {
                         choseColor = "#2e86de"
                     }
@@ -77,32 +93,18 @@ class UserAddActivity : AppCompatActivity() {
                 }
             }
         }
-
-
-
         binding.saveUser.setOnClickListener {
-            var name = ""
-            var follower = 0
-            if (binding.userName.text != null) {
-                name = binding.userName.text.toString()
-            } else {
-                name = "dsad"
-            }
-            if (binding.userFollowers != null) {
-                follower = binding.userFollowers.text.toString().toInt()
-            } else {
-                follower = 0
-            }
-            val usermodel = UserModel(
-                imageIndex = choseImage, backgroundColor = choseColor,
-                userName = name,
-                userFollowers = follower
-            )
-            UserCache.addUser(usermodel)
-            finish()
+           val intIndex=UserCache.getSharedEditId(name)
+            backgroundUser=choseColor
+            imageUser=choseImage
+            nameUser= binding.userName.text.toString()
+            followers=binding.userFollowers.text.toString().toInt()
+            Toast.makeText(this, "$followers", Toast.LENGTH_SHORT).show()
 
+            var usermodel=UserModel(imageIndex = imageUser, backgroundColor = backgroundUser, userName = nameUser, userFollowers = followers)
+
+            UserCache.editUserSaver(usermodel,intIndex)
+            finish()
         }
     }
-
-
 }
--- app/src/main/java/uz/fozilbek/lesson17/ui/MainActivity.kt
+++ app/src/main/java/uz/fozilbek/lesson17/ui/MainActivity.kt
@@ -1,8 +1,16 @@
 package uz.fozilbek.lesson17.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import uz.fozilbek.lesson17.R
 import uz.fozilbek.lesson17.core.data.UserModel
 import uz.fozilbek.lesson17.core.adapter.UsersAdapter
 import uz.fozilbek.lesson17.core.cache.UserCache
@@ -10,9 +18,11 @@ import uz.fozilbek.lesson17.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter = UsersAdapter()
+    private val adapter by lazy {
+        UsersAdapter()
+    }
 
-    private var userList = ArrayList<UserModel>()
+    private var userLists = ArrayList<UserModel>()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -22,13 +32,93 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.userList.adapter = adapter
+         binding.userList.adapter = adapter
 
 
         binding.addUser.setOnClickListener {
             startActivity(Intent(this, UserAddActivity::class.java))
         }
+        adapter.onItemMenuClick=this::onItem
 
+    }
+
+    @SuppressLint("SuspiciousIndentation")
+    private fun onItem(usermodel:UserModel, view: View) {
+//        Toast.makeText(this, "${usermodel.userName}", Toast.LENGTH_SHORT).show()
+        val popupMenu=PopupMenu(this,view)
+        popupMenu.menuInflater.inflate(R.menu.item_menu,popupMenu.menu)
+        popupMenu.show()
+        popupMenu.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.delete_item->{
+
+                    val dialog=AlertDialog.Builder(this)
+                        .setTitle("O'chirish")
+                        .setMessage("Rostanham ma'lumotni o'chirmoqchimisz?")
+                        .setNegativeButton("Xa"){dialog,which->
+                            userLists.remove(usermodel)
+                         adapter.userList=userLists
+
+                            UserCache.getSharedID(usermodel.userName)
+                        }
+                        .setPositiveButton("Yoq"){dialog,which->
+                            dialog.dismiss()
+                        }.create()
+                    dialog.show()
+
+                }
+                R.id.edit_item->{
+                    val dialog=AlertDialog.Builder(this)
+                        .setTitle("O'zgartirish")
+                        .setMessage("Rostanham ma'lumotni o'zgartirmoqchimisiz?")
+                        .setNegativeButton("Xa"){dialog,which->
+                            val intent=Intent(this,EditActivity::class.java)
+                            intent.putExtra("username",usermodel.userName)
+                            intent.putExtra("user_followers",usermodel.userFollowers)
+                            startActivity(intent)
+                        }
+                        .setPositiveButton("Yoq"){dialog,which->
+                            dialog.dismiss()
+                        }.create()
+                    dialog.show()
+
+
+
+
+                }
+            }
+
+
+
+            return@setOnMenuItemClickListener true
+        }
+    }
+
+//    private fun removeCache(usermodel: UserModel) {
+//        getSharedId()
+//
+//    }
+
+//    private fun getSharedId(usermodel: UserModel):Int {
+//
+//    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.sort_by_name->{
+                userLists.sortBy {
+                    it.userName
+                }
+                adapter.userList=userLists
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onResume() {
@@ -37,24 +127,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
+        var users=ArrayList<UserModel>()
+        userLists=UserCache.getUsers()
+        for (i in 0 until userLists.size){
+            if (userLists.get(i).userName!="No name" || userLists.get(i).userFollowers!=0){
+                users.add(UserModel(imageIndex = userLists.get(i).imageIndex,
+                    backgroundColor = userLists.get(i).backgroundColor,
+                    userName = userLists.get(i).userName,
+                    userFollowers =userLists.get(i).userFollowers ))
+            }
+        }
+        userLists.clear()
+        userLists=users
 
-        userList=UserCache.getUsers()
-
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-//        userList.add(UserModel(5, "#ff00ff", "Ibragimov Sherzod", 935))
-//        userList.add(UserModel(3, "#fff0ff", "Jo'rayev Bekzod", 200))
-        adapter.userList=userList
+        adapter.userList=userLists
     }
+fun showDialogDelete(){
 
+
+}
 }
